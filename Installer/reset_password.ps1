9e4e0845c565efcc6aee6b46dfdfe1ef9ad6d863
saitheodore@gmail.com <saitheodore@gmail.com>
2015-08-26 16:03:41 +0530

    Added the changed script to the installer location

--- Installer/reset_password.ps1
+++ Installer/reset_password.ps1
@@ -7,5 +7,16 @@ Param(
    [string]$password
 )
 
-Set-ADAccountPassword -Identity $employeeID -NewPassword (ConvertTo-SecureString $password -AsPlainText -force) -Reset
-Write-Output "$employeeID,Success"
+$filter = 'SamAccountName -like "*' + $employeeID + '*"'
+$user = Get-ADUser -Filter $filter
+
+if($user -eq $null -or $user.SamAccountName -eq $null -or $user.SamAccountName -eq "")
+{
+    throw "User with id $employeeID could not be found"
+}
+else
+{
+    Set-ADAccountPassword -Identity $user.SamAccountName -NewPassword (ConvertTo-SecureString $password -AsPlainText -force) -Reset
+}
+
+#Write-Output $user.SamAccountName
